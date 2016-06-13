2c68316e5369c4e52526a6cb4e7c6004946484b4
Rajiv Verma <rajiv@webguy.in>
2016-06-13 17:03:08 +0530

    Added some stuff in the css

--- functions.php
+++ functions.php
@@ -17,14 +17,14 @@ function sidebar_widgets_init() {
 		'before_title'  => '<h3 class="rounded">',
 		'after_title'   => '</h3>',
 	) );
-register_sidebar( array(
-		'name'          => 'Top Right sidebar',
-		'id'            => 'top-sm-widget-area',
-		'before_widget' => '<div class="top-right-widget-area">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="rounded">',
-		'after_title'   => '</h3>',
-	) );
+	register_sidebar( array(
+			'name'          => 'Top Right sidebar',
+			'id'            => 'top-sm-widget-area',
+			'before_widget' => '<div class="top-right-widget-area">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="rounded">',
+			'after_title'   => '</h3>',
+		) );
 /*Add additional sidebar(s) below this*/
 }
 add_action( 'widgets_init', 'sidebar_widgets_init' );
