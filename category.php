2f54a5aa4b1967cf2bd0478adda5511fda9f5589
Rajiv Verma <rajiv@webguy.in>
2016-06-14 00:14:29 +0530

    Added category and further beautification changes

--- category.php
+++ category.php
@@ -2,52 +2,55 @@
 /**
 * A Simple Category Template
 */
-
 get_header(); ?> 
-
-<section id="primary" class="site-content">
-<div id="content div-main-data-wrapper" role="main">
-
-<?php 
-// Check if there are any posts to display
-if ( have_posts() ) : ?>
-
-<header class="archive-header">
-<h1 class="archive-title">Category: <?php single_cat_title( '', false ); ?></h1>
-
-
-<?php
-// Display optional category description
- if ( category_description() ) : ?>
-<div class="archive-meta"><?php echo category_description(); ?></div>
-<?php endif; ?>
-</header>
-
-<?php
-
-// The Loop
-while ( have_posts() ) : the_post(); ?>
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-
-<div class="entry">
-<?php the_content(); ?>
-
- <p class="postmetadata"><?php
-  comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
-?></p>
+<div id="content div-main-data-wrapper" role="main" class="div-main-data-wrapper">
+<div class="container text-left" id="main-section">
+		<div class="row">
+			<div class="col-sm-9">
+				<?php 
+				// Check if there are any posts to display
+				if ( have_posts() ) : ?>
+					
+					<header class="archive-header">
+						<h1 class="archive-title">Category: <?php single_cat_title( '', true ); ?></h1>
+						<?php
+						// Display optional category description
+						 if ( category_description() ) : ?>
+							<div class="archive-meta"><?php echo category_description(); ?></div>
+						<?php endif; ?>
+					</header>
+					<?php
+					// The Loop
+					while ( have_posts() ) : the_post(); ?>
+						<div class='blog-post'>
+						<h2><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
+						<div class='blogpost-meta'>
+							<span class='glyphicon glyphicon-user'><i class='meta-beautify'>By <?php the_author_posts_link(); ?>&nbsp;</i></span>
+							<span class='glyphicon glyphicon-calendar'><i class='meta-beautify'>&nbsp;<?php the_time('F jS Y'); ?>&nbsp;</i></span>
+							<span class='glyphicon glyphicon-comment'><i class='meta-beautify'>&nbsp;  <a href='<?php comments_link(); ?>' class="comments"> <?php comments_number();?></a>  </i></span>
+							<span class='glyphicon glyphicon-tags'><i class='meta-beautify'>&nbsp;<?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></i></span>
+						</div>
+						<?php if(has_post_thumbnail()) { ?>
+							<div class='featured-image'>
+								<?php the_post_thumbnail( 'medium_large' ); ?>
+							</div>
+						<?php } ?>
+						<div class='blog-post-text'>
+							<?php the_excerpt(); ?>
+						</div>
+						<div class='div-blog-bottom'>
+							<a href="<?php the_permalink(); ?>" class="btn btn-default btn-lg btn-read-more">
+						    	<span class="glyphicon glyphicon glyphicon-check"></span>Read More 
+					        </a>
+						</div>
+					</div>
+					<?php endwhile; 
+				else: ?>
+					<p>Sorry, no posts matched your criteria.</p>
+				<?php endif; ?>
+			</div>
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
 </div>
-
-<?php endwhile; 
-
-else: ?>
-<p>Sorry, no posts matched your criteria.</p>
-
-
-<?php endif; ?>
-</div>
-</section>
-
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div class="col-sm-3">
 	<?php if ( is_active_sidebar( 'right_sidebar_one' ) ) : ?>
-		<div id="primary-sidebar" class="primary-sidebar" role="complementary">
+		<div id="primary-sidebar" class="primary-sidebar right-widgets-sidebar" role="complementary">
 			<?php dynamic_sidebar( 'right_sidebar_one' ); ?>
 		</div><!-- #primary-sidebar -->
 	<?php endif; ?>
