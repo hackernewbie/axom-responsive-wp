ca3b511f040fc91a32cf10fe7866b253bef01c28
Rajiv Verma <rajiv@webguy.in>
2016-07-04 18:58:58 +0530

    Added OG

--- functions.php
+++ functions.php
@@ -55,4 +55,23 @@ function my_show_columns($name) {
             echo $views;
     }
 }
+
+/******************************FB OG *********************************/
+function insert_image_src_rel_in_head() {
+	global $post;
+	if ( !is_singular()) //if it is not a post or a page
+		return;
+	if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+		$default_image="http://goandwander.com/images/default.jpg"; //replace this with a default image on your server or an image in your media library
+		echo '<meta property="og:image" content="' . $default_image . '"/>';
+	}
+	else{
+		$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+		echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+	}
+	echo "
+";
+}
+add_action( 'wp_head', 'insert_image_src_rel_in_head', 5 );
+/******************************FB OG *********************************/
 ?>
